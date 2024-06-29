130c557f9b7917cd524457aa7f8bf69f1d287bc6
Abhinav kar <karabhinav24@gmail.com>
2024-06-29 12:16:54 +0530

    Injected-Dependency-Autowired

--- src/main/java/com/example/backend_grade1/student/StudentController.java
+++ src/main/java/com/example/backend_grade1/student/StudentController.java
@@ -1,5 +1,6 @@
 package com.example.backend_grade1.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +10,12 @@ import java.util.List;
 @RestController
 @RequestMapping(path="api/v1/student")
 public class StudentController {
+
 	private final StudentService studentService;
-	public StudentController(StudentService studentService){
+
+
+	@Autowired
+	public StudentController(StudentService studentService){//dependency Injection for this we have used Autowired
 		this.studentService=studentService;
 	}
 
--- src/main/java/com/example/backend_grade1/student/StudentService.java
+++ src/main/java/com/example/backend_grade1/student/StudentService.java
@@ -1,8 +1,11 @@
 package com.example.backend_grade1.student;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class StudentService {
+
     public List<Student> getStudentInfo(){
         return List.of(new Student(
                 1L,
